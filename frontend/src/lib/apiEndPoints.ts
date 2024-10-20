03ddc20054fdee38036fdf6bc4dfbe03b11ce938
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-10-21 01:43:51 +0530

    base url

---
+++ frontend/src/lib/apiEndPoints.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const API_URL = BASE_URL + "/api";
+
+export const LOGIN_URL = process.env.NEXT_PUBLIC_APP_URL + "/auth/login";
---
+++ frontend/src/lib/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+  API_URL: process.env.NEXT_PUBLIC_APP_URL,
+  BACKEND_URL: process.env.NEXT_PUBLIC_Ba_URL,
+};
