052df0017fec4e23b4c97cc52130a28bb7d5a02e
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-10-21 17:41:13 +0530

    dashboard nav

--- frontend/src/components/DashNav.tsx
+++ frontend/src/components/DashNav.tsx
@@ -5,7 +5,7 @@ import ProfileMenu from "./auth/ProfileMenu";
 export default function DashNav() {
   return (
     <nav className="py-2 px-6 flex justify-between items-center bg-white shadow-sm">
-      <h1 className="text-xl md:text-2xl font-extrabold">Blaze CHat</h1>
+      <h1 className="text-xl md:text-2xl font-extrabold">Blaze Chat</h1>
       <div className="flex items-center space-x-2 md:space-x-6 text-gray-700">
         <ProfileMenu name="Zeshan" />
       </div>
