d721c65609a8ef5df72828ac59070d0a49285af3
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-10-23 13:31:09 +0530

    chat group router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,28 @@
 import { Router } from "express";
 import { loginUser } from "../controller/userController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { chatGroupStore } from "../controller/chatGroupController.js";
+import {
+  chatGroupDestroy,
+  chatGroupIndex,
+  chatGroupShow,
+  chatGroupStore,
+  chatGroupupdate,
+} from "../controller/chatGroupController.js";
 
 const router = Router();
 
+//login
 router.post("/auth/login", loginUser);
 
+//chat group
 router.post("/chatgroup", authMiddleware, chatGroupStore);
+
+router.get("/chatgroup/:id", authMiddleware, chatGroupShow);
+
+router.get("/chatgroup", authMiddleware, chatGroupIndex);
+
+router.put("/chatgroup/:id", authMiddleware, chatGroupupdate);
+
+router.delete("/chatgroup/:id", authMiddleware, chatGroupDestroy);
 
 export default router;
