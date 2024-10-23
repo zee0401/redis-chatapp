43d81f238ad85f01993c2bd1c5fbdfc3b829c680
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-10-23 13:00:12 +0530

    fetch all group controller created

--- server/src/controller/chatGroupController.ts
+++ server/src/controller/chatGroupController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../config/db.config.js";
+import { create } from "domain";
 
 export const chatGroupStore = async (req: Request, res: Response) => {
   try {
@@ -14,6 +15,28 @@ export const chatGroupStore = async (req: Request, res: Response) => {
       },
     });
     return res.json({ message: "Chat Group created successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const chatGroupIndex = async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const user = req.user;
+
+    const groups = await prisma.chatGroup.findMany({
+      where: {
+        user_id: parseInt(user.id),
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return res.json({
+      message: "Chat Group fetched successfully!",
+      data: groups,
+    });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }
