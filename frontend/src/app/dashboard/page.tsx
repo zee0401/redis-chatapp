cba04bb749d2c6c9a7abdc656b545521e868931b
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-10-24 17:00:42 +0530

    fixes

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -6,10 +6,15 @@ import {
   CustomSession,
 } from "@/app/api/auth/[...nextauth]/options";
 import CreateChat from "@/components/groupChat/CreateChat";
+import { chatGroupType } from "../../types";
+import { fetchChatGroups } from "../fetch/fetchChatGroup";
 
 const dashboard = async () => {
   const session: CustomSession | null = await getServerSession(authOptions);
 
+  const groups: Array<chatGroupType> | [] = await fetchChatGroups(
+    session?.user?.token
+  );
   return (
     <div>
       <DashNav
@@ -19,6 +24,13 @@ const dashboard = async () => {
       <div className="container">
         <div className="mt-6 text-end">
           <CreateChat user={session?.user!} />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {groups.length > 0 &&
+            groups.map((item, index) => (
+              <GroupChatCard group={item} key={index} user={session?.user!} />
+            ))}
         </div>
       </div>
     </div>
---
+++ frontend/src/types.ts
@@ -0,0 +1,7 @@
+export interface chatGroupType {
+  id: string;
+  user_id: number;
+  passcode: string;
+  title: string;
+  created_at: string;
+}
