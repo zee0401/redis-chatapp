cedfd9595e2edd831433365e51887b4c27161ae3
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-10-30 12:25:51 +0530

    chat groupuser controller created

---
+++ server/src/controller/chatGroupUserController.ts
@@ -0,0 +1,32 @@
+import { Response, Request } from "express";
+import prisma from "../config/db.config.js";
+
+export const chatGroupUserIndex = async (req: Request, res: Response) => {
+  try {
+    const { group_id } = req.query;
+    const users = await prisma.groupUsers.findMany({
+      where: {
+        group_id: group_id as string,
+      },
+    });
+    return res.json({ message: "Data fetched Successfully", data: users });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong. Please try again!" });
+  }
+};
+
+export const chatGroupUserCreate = async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const user = await prisma.groupUsers.create({
+      data: body,
+    });
+    return res.json({ message: "User created successfully!", data: user });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong.please try again!" });
+  }
+};
