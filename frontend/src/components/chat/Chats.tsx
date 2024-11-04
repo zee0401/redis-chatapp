4c09b27002edb9e1a11a4ee39ae4e7c1a476a570
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-04 11:12:48 +0530

    chats component

---
+++ frontend/src/components/chat/Chats.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { connectSocket } from "@/lib/socket.config";
+import { v4 as uuidv4 } from "uuid";
+export default function Chats({
+  group,
+  oldMessages,
+  chatUser,
+}: {
+  group: ChatGroupType;
+  oldMessages: Array<MessageType> | [];
+  chatUser?: GroupChatUserType;
+}) {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<Array<MessageType>>(oldMessages);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const socket = useMemo(() => {
+    const socket = connectSocket();
+    socket.auth = {
+      room: group.id,
+    };
+    return socket.connect();
+  }, []);
+  useEffect(() => {
+    socket.on("message", (data: MessageType) => {
+      console.log("The message is", data);
+      setMessages((prevMessages) => [...prevMessages, data]);
+      scrollToBottom();
+    });
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const payload: MessageType = {
+      id: uuidv4(),
+      message: message,
+      name: chatUser?.name ?? "Unknown",
+      created_at: new Date().toISOString(),
+      group_id: group.id,
+    };
+    socket.emit("message", payload);
+    setMessage("");
+    setMessages([...messages, payload]);
+  };
+
+  return (
+    <div className="flex flex-col h-[94vh]  p-4">
+      <div className="flex-1 overflow-y-auto flex flex-col-reverse">
+        <div ref={messagesEndRef} />
+        <div className="flex flex-col gap-2">
+          {messages.map((message) => (
+            <div
+              key={message.id}
+              className={`max-w-sm rounded-lg p-2 ${
+                message.name === chatUser?.name
+                  ? "bg-gradient-to-r from-blue-400 to-blue-600  text-white self-end"
+                  : "bg-gradient-to-r from-gray-200 to-gray-300 text-black self-start"
+              }`}
+            >
+              {message.message}
+            </div>
+          ))}
+        </div>
+      </div>
+      <form onSubmit={handleSubmit} className="mt-2 flex items-center">
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={message}
+          className="flex-1 p-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+}
